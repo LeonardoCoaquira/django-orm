195b12fadc7d67bd3561357281d888ed4eafc064
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2023-04-17 10:16:33 -0500

    parte 1

--- practica/examen/models.py
+++ practica/examen/models.py
@@ -1,23 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Examen(models.Model):
-    titulo = models.CharField(max_length=255)
-    # Otros campos necesarios para el examen, como fecha, duración, etc.
-
 class Pregunta(models.Model):
-    examen = models.ForeignKey(Examen, on_delete=models.CASCADE)
-    texto = models.CharField(max_length=255)
-    # Otros campos necesarios para la pregunta, como tipo de respuesta, etc.
+    pregunta_texto = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date_published')
 
-class Alternativa(models.Model):
+    def __str__(self):
+        return self.pregunta_texto
+
+class Opcion(models.Model):
     pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
-    texto = models.CharField(max_length=255)
-    es_correcta = models.BooleanField(default=False)
-    # Otros campos necesarios para la alternativa, como valor, etc.
-
-class Nota(models.Model):
-    examen = models.ForeignKey(Examen, on_delete=models.CASCADE)
-    alumno = models.CharField(max_length=255)
-    nota = models.FloatField()
-    # Otros campos necesarios para la nota del alumno, como fecha, etc.
+    opcion_texto = models.CharField(max_length=255)
+    puntos = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.opcion_texto
--- practica/examen/views.py
+++ practica/examen/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from .models import Pregunta, Opcion
 # Create your views here.
+
+def index(request):
+    lista_pregunta = Pregunta.objects.all()
+    context = {
+        'preguntas' : lista_pregunta
+    }
+    return render(request, 'index.html',context)
+
+def resolver(request):
+    preguntas = Pregunta.objects.all()
+    nota = 0
+    for pregunta in preguntas:
+        nota = int(request.POST['pregunta_'+str(pregunta.id)])
+    
+    context = {
+        'nota' : nota
+    }
+
+    return render(request, 'resultados.html',context)
